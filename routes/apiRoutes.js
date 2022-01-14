9fb983a95b1a501daae5681da710dbc2db813965
Mike Pizzica <mike.pizzica@gmail.com>
2022-01-14 11:52:35 -0500

    add apiRoutes and update server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,69 @@
-const router = require("express").Router()
-const workout = require("../models/workoutPlan.js")
+const router = require("express").Router();
+const Workout = require("../models/workoutPlan.js");
 
 // create routes for api.js
 // look at directions use aggregates
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).res.json(err)
+        })
+})
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                },
+            },
+        },
+    ])
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+});
+
+router.put("/api/workouts/:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        {_id: params.id},
+        {$push: {exercises: body}},
+        {new: true}
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).res.json(err)
+        })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:
+                    {$sum: 'exercises.duration'},
+                totalWeight:
+                    {$sum: '$exercises.weight'}
+            }
+        }
+    ])
+        .limit(10)
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const viewRoutes = require("./routes/viewRoutes.js");
+const apiRoutes = require("./routes/apiRoutes");
 
 // added this from "MongoAtlas-Deploy" directions in "Important" folder
 mongoose.connect(
@@ -28,7 +29,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(viewRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
-  console.log("listening on " + PORT)
+  console.log("listening on " + PORT);
 })
